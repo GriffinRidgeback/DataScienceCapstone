4e3e814fedd15b25d6abed277531f2a35388a2a7
GriffinRidgeback <KevinDElia@verizon.net>
2017-03-26 11:52:40 -0400

    Final push for project

--- shinyapp/DataCapstone/server.R
+++ shinyapp/DataCapstone/server.R
@@ -13,10 +13,13 @@ tri.gram.table <- readRDS("./data/tri.gram.table.rds")
 quad.gram.table <- readRDS("./data/quad.gram.table.rds")
 quint.gram.table <- readRDS("./data/quint.gram.table.rds")
 
-source("predictionAlgorithmn.R")
+source("predictionAlgorithm.R")
 
 shinyServer(function(input, output) {
-  userInput <- reactive({
+
+  # userInput <- reactive({
+  userInput <- renderText({
+    
     inputString <- sanitizeInput(input$text)
     
     # Tokenize
@@ -42,120 +45,129 @@ shinyServer(function(input, output) {
     if (l >= 4) {
       # Use quintgram table to find completion for quadgram
       print("PROCESSING QUINTGRAMS")
-      
       results <- searchNGrams(inputString, quint.gram.table)
       if (length(results) != 0) {
         predictions <- processResults(quint.gram.table, results)
+        print(paste("Predictions from quintgram table", predictions))
+        return(predictions)
       }
       else {
         # Use quadgram table to find completion for trigram
         print("PROCESSING QUADGRAMS")
-        
         inputString <- reduceNGram(inputString)
         results <- searchNGrams(inputString, quad.gram.table)
         if (length(results) != 0) {
           predictions <- processResults(quad.gram.table, results)
+          print(paste("Predictions from quadgram table", predictions))
+          return(predictions)
         }
         else {
           # Use trigram table to find completion for bigram
           print("PROCESSING TRIGRAMS")
-          
           inputString <- reduceNGram(inputString)
           results <- searchNGrams(inputString, tri.gram.table)
           if (length(results) != 0) {
             predictions <- processResults(tri.gram.table, results)
+            print(paste("Predictions from trigram table", predictions))
+            return(predictions)
           }
           else {
             # Use bigram table to find completion for unigram
             print("PROCESSING BIGRAMS")
-            
             inputString <- reduceNGram(inputString)
             results <- searchNGrams(inputString, bi.gram.table)
             if (length(results) != 0) {
               predictions <- processResults(bi.gram.table, results)
+              print(paste("Predictions from bigram table", predictions))
+              return(predictions)
             }
             else {
               predictions <- "No prediction possible"
+              return(predictions)
             }
           }
         }
       }
     }
-    
+
     if (l == 3) {
       # Use quadgram table to find completion for trigram
       print("PROCESSING QUADGRAMS")
-      
-      inputString <- reduceNGram(inputString)
       results <- searchNGrams(inputString, quad.gram.table)
       if (length(results) != 0) {
         predictions <- processResults(quad.gram.table, results)
+        print(paste("Predictions from quadgram table", predictions))
+        return(predictions)
       }
       else {
         # Use trigram table to find completion for bigram
         print("PROCESSING TRIGRAMS")
-        
         inputString <- reduceNGram(inputString)
         results <- searchNGrams(inputString, tri.gram.table)
         if (length(results) != 0) {
           predictions <- processResults(tri.gram.table, results)
+          print(paste("Predictions from trigram table", predictions))
+          return(predictions)
         }
         else {
           # Use bigram table to find completion for unigram
           print("PROCESSING BIGRAMS")
-          
           inputString <- reduceNGram(inputString)
           results <- searchNGrams(inputString, bi.gram.table)
           if (length(results) != 0) {
             predictions <- processResults(bi.gram.table, results)
+            print(paste("Predictions from bigram table", predictions))
+            return(predictions)
           }
           else {
             predictions <- "No prediction possible"
+            return(predictions)
           }
         }
       }
     }
-    
+
     if (l == 2) {
       # Use trigram table to find completion for bigram
       print("PROCESSING TRIGRAMS")
-      
-      inputString <- reduceNGram(inputString)
       results <- searchNGrams(inputString, tri.gram.table)
       if (length(results) != 0) {
-        predictions <- processResults(tri.gram.table, results)
+       predictions <- processResults(tri.gram.table, results)
+       print(paste("Predictions from trigram table", predictions))
+       return(predictions)
       }
       else {
         # Use bigram table to find completion for unigram
         print("PROCESSING BIGRAMS")
-        
         inputString <- reduceNGram(inputString)
         results <- searchNGrams(inputString, bi.gram.table)
         if (length(results) != 0) {
           predictions <- processResults(bi.gram.table, results)
+          print(paste("Predictions from bigram table", predictions))
+          return(predictions)
         }
         else {
           predictions <- "No prediction possible"
+          return(predictions)
         }
       }
     }
     
     if (l == 1) {
       # Use bigram table to find completion for unigram
-      print("PROCESSING BIGRAMS")
-      
-      inputString <- reduceNGram(inputString)
       results <- searchNGrams(inputString, bi.gram.table)
       if (length(results) != 0) {
         predictions <- processResults(bi.gram.table, results)
+        return(predictions)
       }
       else {
         predictions <- "No prediction possible"
+        return(predictions)
       }
     }
     
   })
-  
+
   output$prediction <- userInput
   
 })
--- shinyapp/DataCapstone/ui.R
+++ shinyapp/DataCapstone/ui.R
@@ -1,35 +1,44 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
   # Application title
-  titlePanel("Next Word Prediction"),
+  titlePanel("Next Word Prediction Application"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-
-    ),
+  # Show a plot of the generated distribution
+  mainPanel(tabsetPanel(
+    type = "tabs",
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       textInput("text", label = h3("Text input"), value = ""),
-       
-       submitButton("Predict Next Word"),
-       helpText("When you click the button above, you should see",
-                "the output below update to reflect the value you",
-                "entered at the top:"),
-       verbatimTextOutput("prediction")
+    tabPanel("Help & Examples",
+             
+             includeHTML("examples.html")),
+    
+    tabPanel(
+      "Try it!",
+      
+      helpText(
+        br(),
+        "Enter a phrase of any length, consisting of any characters,",
+        br(),
+        " and in lower/mixed/upper case in the box below.",
+        br(),
+        br(),
+        "Then click the button to see the top 1, 2, or 3 predictions!"
+      ),
+      
+      textInput(
+        "text",
+        label = h3("Input your phrase here"),
+        value = ""
+      ),
+      
+      submitButton("Predict Next Word"),
+      
+      br(),
+      
+      verbatimTextOutput("prediction")
+      
     )
-  )
+    
+  ))
 ))
